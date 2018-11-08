bd3ea6139a818cc0c75a3f7320561e5d097c0a06
bbicoon <owlboo114@gmail.com>
2018-11-08 14:45:23 +0900

    commit page

---
+++ src/com/nike/page/Search.java
@@ -0,0 +1,33 @@
+package com.nike.page;
+
+public class Search {
+	
+	private String kind;
+	private String search;
+	
+	public String getKind() {
+		if(this.kind == null || this.kind.equals("")) {
+			this.kind="title";
+		}
+		return kind;
+	}
+	public void setKind(String kind) {
+		this.kind = kind;
+		if(kind == null || kind.equals("")) {
+			this.kind="title";
+		}
+	}
+	public String getSearch() {
+		if(this.search == null) {
+			this.search="";
+		}
+		return search;
+	}
+	public void setSearch(String search) {
+		this.search = search;
+		if(search == null) {
+			this.search="";
+		}
+	}
+
+}
