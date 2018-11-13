f73fc654872021a8b0a61514456e37b7777f55c2
bbicoon <owlboo114@gmail.com>
2018-11-13 17:22:26 +0900

    commit

---
+++ src/com/nike/controller/ReviewController.java
@@ -0,0 +1,62 @@
+package com.nike.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.nike.action.ActionFoward;
+
+/**
+ * Servlet implementation class ReviewController
+ */
+@WebServlet("/ReviewController")
+public class ReviewController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ReviewController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String command = request.getPathInfo();
+		ActionFoward actionFoward = null;
+		
+/*		if(command.equals("/reviewList.do")) {
+			actionFoward = reviewService.selectList(request, response);
+		}else if(command.equals("/reviewSelectOne.do")) {
+			actionFoward= reviewService.selectOne(request, response);
+		}else if(command.equals("/reviewWrite.do")) {
+			actionFoward= reviewService.insert(request, response);
+			
+		}
+*/		
+		
+		if(actionFoward.isCheck()) {
+			RequestDispatcher view = request.getRequestDispatcher(actionFoward.getPath());
+			view.forward(request, response);
+		}else {
+			response.sendRedirect(actionFoward.getPath());
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
