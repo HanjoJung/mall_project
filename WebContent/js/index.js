8d7994cf980906740510e585c9d444e6b0f66c36
justicedie1974 <justicedie197407@gmail.com>
2018-11-16 17:53:45 +0900

    tracking line

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -18,4 +18,31 @@ $(document).ready(function() {
 	$('body').on('hidden.bs.modal', '.modal', function() {
 		$(this).removeData('bs.modal');
 	});
+
+	$("#p1>div").mouseenter(function() {
+		$(this).addClass('hover');
+	});
+	$("#p1>div").mouseleave(function() {
+		$(this).removeClass('hover');
+	});
+
+	$("body").on("click", "[data-click-name]", function(e) {
+		// e.preventDefault();
+		// var target = $(this).attr("target") || '_self';
+		// var href = $(this).attr("href");
+		var name = $(this).data("click-name");
+		var area = $(this).data("click-area");
+		var enable = $(this).data("click-enable");
+		var endPoint = Core.getComponents('component_endpoint');
+		// console.log(target);
+		// console.log(href);
+		// console.log(endPoint);
+		if (enable != false) {
+			endPoint.call('clickEvent', {
+				area : area,
+				name : name
+			});
+		}
+		// alert("잠시 멈춤");
+	});
 });
