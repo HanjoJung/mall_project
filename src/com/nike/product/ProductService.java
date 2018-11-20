443e69101b148a7098e503c42298981df1665a21
Hottory <gkffndnl123@naver.com>
2018-11-20 17:48:16 +0900

    t

--- src/com/nike/product/ProductService.java
+++ src/com/nike/product/ProductService.java
@@ -18,53 +18,51 @@ import com.oreilly.servlet.MultipartRequest;
 import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
 public class ProductService {
-	
+
 	private ProductDAO productDAO;
 	private FileDAO fileDAO;
-	
+
 	public ProductService() {
 		productDAO = new ProductDAO();
 		fileDAO = new FileDAO();
 	}
-	
+
 	public ActionFoward selectList(HttpServletRequest request, HttpServletResponse response) {
 		ActionFoward actionFoward = new ActionFoward();
-	
-		
-		int curPage=1;
+
+		int curPage = 1;
 		try {
 			curPage = Integer.parseInt(request.getParameter("curPage"));
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
 		String kind = request.getParameter("kind");
-		if(kind == null || kind.equals("")) {
+		if (kind == null || kind.equals("")) {
 			kind = "productCode";
 		}
 		String search = request.getParameter("search");
-		
+
 		MakePager makePager = new MakePager(curPage, search, kind);
 		RowNumber rowNumber = makePager.makeRow();
 
 		try {
-			
+
 			List<ProductDTO> ar = productDAO.selectList(rowNumber);
 			List<FileDTO> far = new ArrayList<>();
-			for(int i=0;i<ar.size();i++) {
+			for (int i = 0; i < ar.size(); i++) {
 				FileDTO fileDTO = fileDAO.selectOne(ar.get(i).getProductCode());
 				far.add(fileDTO);
 			}
-			
-			
+
 			int totalCount = productDAO.getCount(rowNumber.getSearch());
 			Pager pager = makePager.makePage(totalCount);
-			
+
 			request.setAttribute("list", ar);
 			request.setAttribute("file", far);
 			request.setAttribute("pager", pager);
 			request.setAttribute("board", "product");
 			actionFoward.setPath("../WEB-INF/view/product/productList.jsp");
-		
+
 		} catch (Exception e) {
 
 			request.setAttribute("message", "Fail");
@@ -73,21 +71,20 @@ public class ProductService {
 			e.printStackTrace();
 		}
 		actionFoward.setCheck(true);
-		
+
 		return actionFoward;
 	}
-	
-	
+
 	public ActionFoward selectOne(HttpServletRequest request, HttpServletResponse response) {
-	
+
 		ActionFoward actionFoward = new ActionFoward();
 		ProductDTO productDTO = null;
 		FileDTO fileDTO = null;
 		String code = request.getParameter("code");
 		try {
-			productDTO=productDAO.selectOne(code);
+			productDTO = productDAO.selectOne(code);
 			fileDTO = fileDAO.selectOne(code);
-			
+
 			request.setAttribute("fileOne", fileDTO);
 			request.setAttribute("pDTO", productDTO);
 			request.setAttribute("board", "product");
@@ -97,30 +94,29 @@ public class ProductService {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+
 		return actionFoward;
 	}
-	
-	
+
 	public ActionFoward insert(HttpServletRequest request, HttpServletResponse response) {
-		
+
 		ActionFoward actionFoward = new ActionFoward();
 		String method = request.getMethod();
-		if(method.equals("POST")) {
-			String message="fail";
-			String path="./productList.do";
-			//파일의 크기
-			int maxSize=1024*1024*20;
-			//파일 저장공간
+		if (method.equals("POST")) {
+			String message = "fail";
+			String path = "./productList.do";
+			// 파일의 크기
+			int maxSize = 1024 * 1024 * 20;
+			// 파일 저장공간
 			String save = request.getServletContext().getRealPath("upload");
 			System.out.println(save);
 			File file = new File(save);
-			if(!file.exists()) {
+			if (!file.exists()) {
 				file.mkdirs();
-			}//파일이 없으면 파일을 만들기
+			} // 파일이 없으면 파일을 만들기
 			try {
-				MultipartRequest multi = new MultipartRequest(request, save, maxSize, "utf-8", new DefaultFileRenamePolicy());
+				MultipartRequest multi = new MultipartRequest(request, save, maxSize, "utf-8",
+						new DefaultFileRenamePolicy());
 				ProductDTO productDTO = new ProductDTO();
 				productDTO.setProductCode(productDAO.getCode());
 				productDTO.setProductName(multi.getParameter("name"));
@@ -129,107 +125,97 @@ public class ProductService {
 				productDTO.setManufacturerCode(multi.getParameter("mCode"));
 				productDTO.setWriter(multi.getParameter("writer"));
 				productDTO.setContents(multi.getParameter("contents"));
-				
+
 				int result = productDAO.insert(productDTO);
-				if(result>0) {
-					
+				if (result > 0) {
+
 					FileDAO fileDAO = new FileDAO();
 					Enumeration<Object> e = multi.getFileNames();
 					int i = 1;
-					while(e.hasMoreElements()) {
+					while (e.hasMoreElements()) {
 						System.out.println(e);
-						String p =(String)e.nextElement();
+						String p = (String) e.nextElement();
 						System.out.println(p);
 						FileDTO fileDTO = new FileDTO();
-						fileDTO.setPut(i+"");
+						fileDTO.setPut(i + "");
 						fileDTO.setProductCode(productDTO.getProductCode());
 						fileDTO.setFname(multi.getFilesystemName(p));
 						fileDTO.setOname(multi.getOriginalFileName(p));
 						i++;
-						
+
 						fileDAO.insert(fileDTO);
-						
-						
-					
+
 					}
 					message = "Success";
 					actionFoward.setCheck(true);
 					actionFoward.setPath("../WEB-INF/view/common/result.jsp");
-					
-				}else {
+
+				} else {
 					actionFoward.setCheck(true);
 					actionFoward.setPath("../WEB-INF/view/common/result.jsp");
 				}
-				
+
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 			request.setAttribute("message", message);
 			request.setAttribute("path", path);
-			
-			
-		}else {
+
+		} else {
 			request.setAttribute("board", "product");
 			actionFoward.setCheck(true);
 			actionFoward.setPath("../WEB-INF/view/product/productWrite.jsp");
 		}
-		
-		
+
 		return actionFoward;
-		
+
 	}
-	
-	
+
 	public ActionFoward delete(HttpServletRequest request, HttpServletResponse response) {
 		ActionFoward actionFoward = new ActionFoward();
-		
+
 		try {
 			String code = request.getParameter("code");
-			List<FileDTO> ar =fileDAO.selectList(code);
-			int result2 = fileDAO.delete(code);
-			int result=productDAO.delete(code);
-			if(result>0) {
-				String path =request.getServletContext().getRealPath("upload");
+			List<FileDTO> ar = fileDAO.selectList(code);
+			fileDAO.delete(code);
+			int result = productDAO.delete(code);
+			if (result > 0) {
+				String path = request.getServletContext().getRealPath("upload");
 				System.out.println(path);
 				File file = null;
-				for(int i=0;i<ar.size();i++) {
-					
-					file = new File(path,ar.get(i).getFname());
+				for (int i = 0; i < ar.size(); i++) {
+					file = new File(path, ar.get(i).getFname());
 				}
-				
+
 				file.delete();
 				request.setAttribute("message", "Success");
 				request.setAttribute("path", "./productList.do");
-				
-			}else {
+
+			} else {
 				request.setAttribute("message", "Fail");
 				request.setAttribute("path", "./productList.do");
 			}
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
+			request.setAttribute("message", "Fail");
+			request.setAttribute("path", "./productList.do");
 			e.printStackTrace();
 		}
 		actionFoward.setCheck(true);
 		actionFoward.setPath("../WEB-INF/view/common/result.jsp");
-		
+
 		return actionFoward;
 	}
-	
-	
-	
-	
-	
-	
+
 	public ActionFoward update(HttpServletRequest request, HttpServletResponse response) {
 		ActionFoward actionFoward = new ActionFoward();
-		
+
 		String method = request.getMethod();
-		if(method.equals("POST")) {
-			
-		}else {
+		if (method.equals("POST")) {
+
+		} else {
 			try {
 				String code = request.getParameter("code");
-				ProductDTO productDTO=productDAO.selectOne(code);
+				ProductDTO productDTO = productDAO.selectOne(code);
 				FileDAO fileDAO = new FileDAO();
 				FileDTO fileDTO = new FileDTO();
 				fileDTO.setProductCode(code);
@@ -244,11 +230,8 @@ public class ProductService {
 				// TODO: handle exception
 			}
 		}
-		
+
 		return actionFoward;
 	}
-	
-	
-	
 
 }
